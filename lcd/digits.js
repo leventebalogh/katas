a6c2ab672e885d29308920180c982908d0538815
Levente Balogh <balogh.levente.hu@gmail.com>
2018-03-24 07:56:09 +0100

    Digits: remove unnecessary line breaks

--- lcd/digits.js
+++ lcd/digits.js
@@ -1,64 +1,55 @@
-const DIGIT_0 = `
-._.
+const DIGIT_0 =
+`._.
 |.|
-|_|
-`;
+|_|`;
 
-const DIGIT_1 = `
-...
+const DIGIT_1 =
+`...
 ..|
-..|
-`;
+..|`;
 
-const DIGIT_2 = `
-._.
+const DIGIT_2 =
+`._.
 ._|
+|_.`;
+
+const DIGIT_3 =
+`._.
+._|
+._|`;
+
+const DIGIT_4 =
+`...
+|_|
+..|`;
+
+const DIGIT_5 =
+`._.
 |_.
-`;
+._|`;
 
-const DIGIT_3 = `
-._.
-._|
-._|
-`;
-
-const DIGIT_4 = `
-...
-|_|
-..|
-`;
-
-const DIGIT_5 = `
-._.
+const DIGIT_6 =
+`._.
 |_.
-._|
-`;
+|_|`;
 
-const DIGIT_6 = `
-._.
-|_.
-|_|
-`;
-
-const DIGIT_7 = `
-._.
+const DIGIT_7 =
+`._.
 ..|
-..|
-`;
+..|`;
 
-const DIGIT_8 = `
-._.
+const DIGIT_8 =
+`._.
 |_|
-|_|
-`;
+|_|`;
 
-const DIGIT_9 = `
-._.
+const DIGIT_9 =
+`._.
 |_|
-..|
-`;
+..|`;
 
 module.exports = {
+    DIGIT_0,
     DIGIT_1,
     DIGIT_2,
     DIGIT_3,
