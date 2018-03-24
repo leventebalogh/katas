ecdab252a708b54af1ee3911d3194d559dab1bd5
Levente Balogh <balogh.levente.hu@gmail.com>
2018-03-24 07:42:13 +0100

    Digits: add a file for expected outputs

---
+++ lcd/digits.js
@@ -0,0 +1,71 @@
+const DIGIT_0 = `
+._.
+|.|
+|_|
+`;
+
+const DIGIT_1 = `
+...
+..|
+..|
+`;
+
+const DIGIT_2 = `
+._.
+._|
+|_.
+`;
+
+const DIGIT_3 = `
+._.
+._|
+._|
+`;
+
+const DIGIT_4 = `
+...
+|_|
+..|
+`;
+
+const DIGIT_5 = `
+._.
+|_.
+._|
+`;
+
+const DIGIT_6 = `
+._.
+|_.
+|_|
+`;
+
+const DIGIT_7 = `
+._.
+..|
+..|
+`;
+
+const DIGIT_8 = `
+._.
+|_|
+|_|
+`;
+
+const DIGIT_9 = `
+._.
+|_|
+..|
+`;
+
+module.exports = {
+    DIGIT_1,
+    DIGIT_2,
+    DIGIT_3,
+    DIGIT_4,
+    DIGIT_5,
+    DIGIT_6,
+    DIGIT_7,
+    DIGIT_8,
+    DIGIT_9
+};
