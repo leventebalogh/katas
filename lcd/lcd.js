1ff9e4ce82678d543f761257e4d2296c9b58c61f
Levente Balogh <balogh.levente.hu@gmail.com>
2018-03-24 07:56:24 +0100

    LCD: display number 0

--- lcd/lcd.js
+++ lcd/lcd.js
@@ -5,13 +5,21 @@
 
 const COLUMN_NUM = 3;
 const ROW_NUM = 3;
+const CHARACTERS = {
+    PIPE: '|',
+    UNDERSCORE: '_',
+    EMPTY: '.'
+};
+const EMPTY_INDEXES = [
+    [1, 3, 5]
+];
 
 module.exports = function lcd (number) {
     return getCharacterArray()
         .map((character, index) => index)
         .reduce((output, index) => {
             const column = index + 1;
-            const character = '.';
+            const character = getCharacter(column, number);
 
             if (isEndOfRow(column) && !isLastCharacter(column)) {
                 return `${ output }${ character }\n`;
@@ -20,6 +28,24 @@ module.exports = function lcd (number) {
             return `${ output }${ character }`;
         }, '');
 };
+
+function getCharacter (index, number) {
+    const emptyIndexes = EMPTY_INDEXES[number] || [];
+
+    if (emptyIndexes.indexOf(index) >= 0) {
+        return CHARACTERS.EMPTY;
+    }
+
+    if (isEndOfRow(index) || isBeginningOfRow(index)) {
+        return CHARACTERS.PIPE;
+    }
+
+    return CHARACTERS.UNDERSCORE;
+}
+
+function isBeginningOfRow (column) {
+    return (column - 1) % COLUMN_NUM === 0;
+}
 
 function isEndOfRow (column) {
     return column % COLUMN_NUM === 0;
--- lcd/lcd.test.js
+++ lcd/lcd.test.js
@@ -1,12 +1,20 @@
 const test = require('tape');
 const lcd = require('./lcd');
+const {
+    DIGIT_0
+} = require('./digits');
 
-test('LCD', function (t) {
+test('LCD - grid size', function (t) {
     const number = lcd(3);
     const lines = number.split('\n');
     const [line] = lines;
 
     t.equal(lines.length, 3, 'should have 3 rows per digit');
     t.equal(line.length, 3, 'should have 3 columns per digit');
+    t.end();
+});
+
+test('LCD - numbers', function (t) {
+    t.equal(lcd(0), DIGIT_0, 'should be able to display number 0');
     t.end();
 });
